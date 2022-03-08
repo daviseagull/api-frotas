dc6915a3a67f6b885bcf8f942279b648925fba8a
Davi Seagull <dseagull@ibm.com>
2022-03-08 14:39:58 -0300

    chore: initializing project

---
+++ src/main/java/com/lab/engenharia/apifrotas/ApiFrotasApplication.java
@@ -0,0 +1,13 @@
+package com.lab.engenharia.apifrotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFrotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFrotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lab/engenharia/apifrotas/ApiFrotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.engenharia.apifrotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFrotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
